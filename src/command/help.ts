00f2b67da7fb5fd15eae131712d2881ab05e2cdb
raziman <raziman.terra@gmail.com>
2021-06-17 16:10:53 +0800

    update help message

--- src/command/help.ts
+++ src/command/help.ts
@@ -11,20 +11,23 @@ export default {
     MemeBroker is trading bot based on reddit posts on reddit. Trading is 
     limited to only a few subreddits which are \`r/dankmemes\` and \`r/memes\`.
     Meme value is calculated based on its upvote, upvote ratio per age (seconds).
+    The commands may have required or optional arguments, to differentiate
+    \`<>\` is for required argument and \`[]\` is for optional. For example,
+    \`r!buy https://reddit.com/sample 10\`
 
-    **r!help**
+    \`r!help\`
     Show commands usage.
 
-    **r!buy <link>**
+    \`r!buy <link> [unit]\`
     Buy meme from reddit with it's current value.
 
-    **r!sell <link>**
+    \`r!sell <link>\`
     Sell meme from reddit with it's current value.
 
-    **r!profile**
+    \`r!profile\`
     Show user profile with currently holding memes.
 
-    **r!value <link>**
+    \`r!value <link>\`
     Check the current value of the meme.
     `
 
