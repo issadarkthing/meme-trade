eed1951d5d0aea150b3fd4d56c1b5c78c7df2d4c
raziman <raziman.terra@gmail.com>
2021-06-17 22:02:54 +0800

    raise the default balance

--- src/structure/trader/schema.ts
+++ src/structure/trader/schema.ts
@@ -5,6 +5,6 @@ export const TraderSchema = new Schema({
 	userID: String,
 	username: String,
 	banned: { type: Boolean, default: false },
-	balance: { type: Number, default: 0.001 },
+	balance: { type: Number, default: 10 },
 	items: { type: Array, default: [] }
 })
