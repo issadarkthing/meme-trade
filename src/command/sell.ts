e6100ecd0553bcf6b712be8433bf2b69c9aba1bc
raziman <raziman.terra@gmail.com>
2021-06-17 18:31:53 +0800

    implement sell all

--- src/command/sell.ts
+++ src/command/sell.ts
@@ -4,6 +4,9 @@ import { TransactionModel } from "../structure/transaction/model";
 import { noProfileErr } from "../template/error";
 import { Item } from "../structure/item"
 import { format } from "../utils";
+import { ITransactionDocument } from "../structure/transaction/types";
+import { ITraderDocument } from "../structure/trader/types";
+import { oneLine } from "common-tags";
 
 
 export default {
@@ -21,51 +24,71 @@ export default {
 
     if (!index)
       return msg.channel.send("Please provide an item index");
-    else if (!parseInt(index))
+    else if (!parseInt(index) && index !== "all")
       return msg.channel.send("Please provide valid index");
 
 
 		let transactions = await trader.getTransactions()
 		const buyTransaction = transactions[parseInt(index) - 1];
 
-		if (!buyTransaction) {
+    if (index === "all") {
+
+      for (const transaction of transactions) {
+        await sellItem(msg, transaction, trader);
+      }
+
+    } else if (!buyTransaction) {
 			msg.channel.send("No item found");
-			return
-		}
 
+    } else {
+      await sellItem(msg, buyTransaction, trader);
 
-		try {
-      const item = await Item.getItem(buyTransaction.url);
-      const itemValue = item.value;
-			const profit = (itemValue - buyTransaction.value) * buyTransaction.unit;
-			const profitPercent = (profit / buyTransaction.value * 100).toFixed(2);
+    }
 
-			const transaction = new TransactionModel();
-			transaction.userID = trader.userID;
-			transaction.url = item.url;
-			transaction.value = itemValue;
-			transaction.score = item.score;
-			transaction.age = item.age;
-			transaction.operation = "SELL";
-			transaction.created = new Date();
-			transaction.buyTransactionID = buyTransaction.id;
-			transaction.profitMargin = profit;
-      transaction.unit = buyTransaction.unit;
-			transaction.save();
-
-			trader.removeItem(buyTransaction.id);
-
-			trader.balance += itemValue * buyTransaction.unit;
-			trader.save();
-			msg.channel.send(`Successfully sold ${item.url}`);
-
-			const text =
-				`**Sell:** \`${format(itemValue)}\` **Profit:** \`${format(profit)} (${profitPercent}%)\``
-			msg.channel.send(text)
-		} catch (e) {
-			msg.channel.send("There was an error while processing your transaction");
-      console.error(e);
-		}
 
 	}
+}
+
+async function sellItem(
+  msg: Message, 
+  buyTransaction: ITransactionDocument, 
+  trader: ITraderDocument,
+) {
+
+  try {
+    const item = await Item.getItem(buyTransaction.url);
+    const itemValue = item.value;
+    const profit = (itemValue - buyTransaction.value) * buyTransaction.unit;
+    const profitPercent = (profit / buyTransaction.value * 100).toFixed(2);
+
+    const transaction = new TransactionModel();
+    transaction.userID = trader.userID;
+    transaction.url = item.url;
+    transaction.value = itemValue;
+    transaction.score = item.score;
+    transaction.age = item.age;
+    transaction.operation = "SELL";
+    transaction.created = new Date();
+    transaction.buyTransactionID = buyTransaction.id;
+    transaction.profitMargin = profit;
+    transaction.unit = buyTransaction.unit;
+    transaction.save();
+
+    trader.removeItem(buyTransaction.id);
+
+    trader.balance += itemValue * buyTransaction.unit;
+    await trader.save();
+    msg.channel.send(`Successfully sold ${item.url}`);
+
+    const totalValue = format(itemValue * buyTransaction.unit);
+
+    const text = oneLine`
+      **Sell:** \`${totalValue}\` 
+      **Profit:** \`${format(profit)} (${profitPercent}%)\``;
+
+    msg.channel.send(text);
+  } catch (e) {
+    msg.channel.send("There was an error while processing your transaction");
+    console.error(e);
+  }
 }
