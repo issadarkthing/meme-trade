1b634464114e60fdb99ebb2c6fd6a7e8f8908cd7
raziman <raziman@tutanota.com>
2022-07-01 18:00:30 +0800

    add dotenv

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,22 @@ import Discord from "discord.js"
 import path from "path"
 import mongoose from "mongoose"
 import { walk } from "./utils"
+import { config } from "dotenv";
+
+config();
 
 export interface Command {
-	name: string;
+  name: string;
   description: string;
   args?: string;
-	alias?: string;
-	exec: (msg: Discord.Message, args: string[]) => void;
+  alias?: string;
+  exec: (msg: Discord.Message, args: string[]) => void;
 }
 
 const uri = process.env.MONGODB_TOKEN
 
 if (!uri) {
-	throw new Error("no mongodb uri found")
+  throw new Error("no mongodb uri found")
 }
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -32,37 +35,37 @@ export const MAX_ITEM = 3;
 
 walk(path.resolve(__dirname, "command"), (err, res) => {
 
-	if (err || !res) {
-		console.error(err)
-		return
-	}
-	
-	const command = require(res)
-	commands.set(command.default.name, command.default)	
+  if (err || !res) {
+    console.error(err)
+    return
+  }
+
+  const command = require(res)
+  commands.set(command.default.name, command.default)	
 })
 
 bot.on("ready", () => {
-	console.log(bot.user?.username + " is ready")
-	bot.user?.setActivity("memes", { type: "WATCHING" })
+  console.log(bot.user?.username + " is ready")
+  bot.user?.setActivity("memes", { type: "WATCHING" })
 })
 
 bot.on("message", msg => {
-	const [prefix, ...args] = msg.content.split(" ").map(x => x.trim())
+  const [prefix, ...args] = msg.content.split(" ").map(x => x.trim())
 
-	if (!prefix.startsWith("r!") || msg.author.bot) {
-		return
-	}
+  if (!prefix.startsWith("r!") || msg.author.bot) {
+    return
+  }
 
-	const commandName = prefix.replace(/^r!/, "")
-	const command = commands.find(x => x.alias === commandName) 
+  const commandName = prefix.replace(/^r!/, "")
+  const command = commands.find(x => x.alias === commandName) 
     || commands.get(commandName);
 
-	if (!command) {
-		msg.channel.send("invalid command")
-		return
-	}
+  if (!command) {
+    msg.channel.send("invalid command")
+    return
+  }
 
-	command.exec(msg, args)
+  command.exec(msg, args)
 })
 
 
